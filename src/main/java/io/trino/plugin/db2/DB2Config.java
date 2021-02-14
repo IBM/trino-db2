d45d8fd38fc4deb981e37051f0c6a9d52ffe7810
Ke Zhu <kzhu@us.ibm.com>
2021-02-14 16:45:28 -0500

    Rename to support trino

--- src/main/java/io/trino/plugin/db2/DB2Config.java
+++ src/main/java/io/trino/plugin/db2/DB2Config.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.prestosql.plugin.db2;
+package io.trino.plugin.db2;
 
 import io.airlift.configuration.Config;
 import io.airlift.configuration.ConfigDescription;
